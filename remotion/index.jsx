cb0a67d9791beace65c5b192418bee593ea12ffa
wecreates <147456350+wecreates@users.noreply.github.com>
2026-09-23 22:42:46 -0700

    Register Episode 1 V5 proof composition

--- remotion/index.jsx
+++ remotion/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Composition, registerRoot} from "remotion";
 import {Episode1Motion} from "./Episode1Motion";
 import {Episode1V4} from "./Episode1V4";
+import {Episode1V5Proof} from "./Episode1V5Proof";
 
 const Root = () => (
   <>
@@ -22,6 +23,14 @@ const Root = () => (
       width={1280}
       height={720}
       defaultProps={{audioDuration:316}}
+    />
+    <Composition
+      id="Episode1V5Proof"
+      component={Episode1V5Proof}
+      durationInFrames={900}
+      fps={30}
+      width={1280}
+      height={720}
     />
   </>
 );
